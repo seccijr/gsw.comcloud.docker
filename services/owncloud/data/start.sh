936955a49a555002314b217efef1cabab2f2c014
Carlos I. Pérez Sechi <secci.jr@gmail.com>
2015-12-03 23:26:31 +0100

    Added support for cas to owncloud

--- services/owncloud/data/start.sh
+++ services/owncloud/data/start.sh
@@ -2,10 +2,10 @@
 
 puppet apply /tmp/manifest.pp
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
+if [[ $1 == "-d"  ]]; then
+    while true; do sleep 1000; done
 fi
 
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
+if [[ $1 == "-bash"  ]]; then
+    /bin/bash
 fi
